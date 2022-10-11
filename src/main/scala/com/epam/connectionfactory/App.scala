3287653a5adb87619a59b87c30a5f6da4119ea25
Yury Bakhmutski <Yury_Bakhmutski@epam.com>
2022-10-11 19:16:51 +0300

    Adding xml reading spark job

--- src/main/scala/com/epam/connectionfactory/App.scala
+++ src/main/scala/com/epam/connectionfactory/App.scala
@@ -1,15 +1,27 @@
 package com.epam.connectionfactory
 
+import com.databricks.spark.xml.XmlDataFrameReader
+import org.apache.spark.sql.SparkSession
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+
+  def main(args: Array[String]) {
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("sf20")
+      .getOrCreate()
+
+    val df = spark.read
+      .option("rowTag", "x:books")
+      .xml(getClass.getResource("/input_file.xml").toString)
+
+    df.printSchema()
+
+    df.show(1, false)
   }
 
 }
