9c6dbbd70860995693057c1474973b24ff24265a
Yury Bakhmutski <Yury_Bakhmutski@epam.com>
2022-10-11 19:11:04 +0300

    Initial commit

---
+++ src/test/scala/com/epam/connectionfactory/AppTest.scala
@@ -0,0 +1,17 @@
+package com.epam.connectionfactory
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
