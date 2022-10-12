20adad352f6365077535355d8ff1a0c8802e5e2a
Yury Bakhmutski <Yury_Bakhmutski@epam.com>
2022-10-12 19:00:51 +0300

    Made project being built, cleaned pom file, cleaned and added logging

--- src/main/scala/com/epam/connectionfactory/PointToRun.scala
+++ src/main/scala/com/epam/connectionfactory/PointToRun.scala
@@ -2,22 +2,28 @@ package com.epam.connectionfactory
 
 import com.databricks.spark.xml.XmlDataFrameReader
 import org.apache.spark.sql.SparkSession
+import org.slf4j.{Logger, LoggerFactory}
 
 /**
  * @author ${user.name}
  */
-object App {
+object PointToRun {
 
   def main(args: Array[String]) {
+    val log: Logger = LoggerFactory.getLogger(getClass)
 
     val spark: SparkSession = SparkSession.builder()
       .master("local[1]")
-      .appName("sf20")
+      .appName("Connection-Factory-2.0")
       .getOrCreate()
+
+    val path = if (args.nonEmpty) args(0) else getClass.getResource("/input_file.xml").toString
+
+    log.info(s"xml file path: $path")
 
     val df = spark.read
       .option("rowTag", "x:books")
-      .xml(getClass.getResource("/input_file.xml").toString)
+      .xml(path)
 
     df.printSchema()
 
